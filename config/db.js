1ab0dbb076450fca4a1980a300463595cbbe5dc4
rabin9840 <rabinshrestha984156@gmail.com>
2023-06-27 18:48:41 +0545

    setup database connection

--- config/db.js
+++ config/db.js
@@ -1,4 +1,21 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+const connectDB = async () => { 
+    try {
+        const dbConn = await mongoose.connect(process.env.Mongo_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+        console.log(`MongoDB connected: ${dbConn.connection.host}`);
+        
+    } catch (error) {
+        console.log(error);
+        process.exit(1)
+        
+    }
+}
 
 // load config file
 dotenv.config({ path: './config/config.env' });
