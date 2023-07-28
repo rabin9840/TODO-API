5500179225c06628d499060bdfc00ed2723fef08
rabin9840 <rabinshrestha984156@gmail.com>
2023-07-28 11:37:55 +0545

    created user model

---
+++ application/models/users.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const usersSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+},
+);
+
+
+const User = mongoose.model('User', usersSchema);
+
+module.exports = User;
